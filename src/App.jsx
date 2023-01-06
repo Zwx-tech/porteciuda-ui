f2d046a14be0c3f48d319746337d28b3ab198ff4
Zwx-tech <71787137+Zwx-tech@users.noreply.github.com>
2023-01-06 13:18:02 +0100

    app scuff folding
    We decided to use react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import UserInput from './components/UserInput'
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div className="app">
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/UserInput.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+const UserInput = () => {  
+
+  return (
+    <div className="user-input">
+      
+    </div>
+  )
+}
+
+export default UserInput;
