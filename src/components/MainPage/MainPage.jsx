ab0c5ceaf4cc75aa1c48e20b175ffe7cdc9d94d0
Zwx <staszekd2020@gmail.com>
2023-01-09 17:54:12 +0100

    added some scuff folding for UserInput component

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -3,8 +3,13 @@ import "./MainPage.css"
 import dnaImage from "./../../assets/MainPage/dna.png"
 import dnaCover from "./../../assets/MainPage/dna-cover.png"
 import dnaText from "./../../assets/MainPage/text.png"
+import UserInput from '../UserInput'
+import { useState } from 'react'
 
 const MainPage = () => {
+
+  const [menuState, setMenuState] = useState(false);
+
   return (
     <div className="main-page">
       <img src={dnaImage} alt="" className="main-page__bg --blurred" />
@@ -15,8 +20,9 @@ const MainPage = () => {
         <button className="main-page__button --left">Wpisz</button>
         <button className="main-page__button --right">Wgraj Plik</button>
       </div>
+      <UserInput />
     </div>
   )
 }
 
-export default MainPage
+export default MainPag
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -3,8 +3,11 @@ import { useState } from 'react'
 const UserInput = () => {  
 
   return (
-    <div className="user-input">
-      
+    <div className='user-input'>
+      <div className='user-input__buttons'>
+        <button className='user-input__button'></button>
+        <button className='user-input__button --light'></button>
+      </div>
     </div>
   )
 }
